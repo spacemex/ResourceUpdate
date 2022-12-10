d6c6f80d5633d995945edc89f0e9d860d14c01a9
spacemex <devenshona@gmail.com>
2022-12-09 23:38:51 -0600

    Added Copper Axe

--- src/main/java/net/spacemex/resourceupdate/items/ModItems.java
+++ src/main/java/net/spacemex/resourceupdate/items/ModItems.java
@@ -1,16 +1,24 @@
 package net.spacemex.resourceupdate.items;
 
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.spacemex.resourceupdate.ResourceUpdateMod;
+import net.spacemex.resourceupdate.creative.ModCreativeModeTab;
+import net.spacemex.resourceupdate.teirs.ModTiers;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ResourceUpdateMod.MODID);
 
 
+    //Tools
+    public static final RegistryObject<Item> COPPER_AXE = ITEMS.register("copper_axe",
+            ()-> new AxeItem(ModTiers.Copper, 0,0,
+                    new Item.Properties().tab(ModCreativeModeTab.RESOURCE_UPDATE_TAB)));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
